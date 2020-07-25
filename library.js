f4981aab735c4797c087ae3c221f33c762eba329
o2sevruk <o2sevruk@gmail.com>
2020-07-25 16:00:20 +0300

    Redux implementation ready.

---
+++ library.js
@@ -0,0 +1,34 @@
+function createStore(reducer) {
+	// The store should have four parts
+	// 1. The state
+	// 2. Get the state.
+	// 3. Listen to changes on the state.
+	// 4. Update the state
+	
+	let state,
+		listeners = [];
+	
+	// Methods
+	const getState = () => state;
+	
+	const subscribe = (listener) => {
+		listeners.push(listener);
+		
+		return () => {
+			listeners = listeners.filter((l) => l !== listener);
+		}
+	}
+	
+	const dispatch = (action) => {
+		state = reducer(state, action);
+		listeners.forEach((listener) => listener());
+	}
+	
+	// /Methods
+	
+	return {
+		getState,
+		subscribe,
+		dispatch
+	}
+}
